91999c1ddc6d72c36a056571ece421fefcc6be06
fadi omar <fadeomar2015@gmail.com>
2019-09-07 22:18:25 +0300

    edit getuser query

--- server/database/queries/getUser.js
+++ server/database/queries/getUser.js
@@ -1,1 +1,11 @@
-// Write a query to get the user and their password from the database
+const dbConnection = require("../config/connection");
+
+const getUser = email => {
+  const sql = {
+    text: "SELECT * FROM user WHERE email LIKE ($1);",
+    values: [email]
+  };
+  return dbConnection.query(sql);
+};
+
+module.exports = { getUser };
